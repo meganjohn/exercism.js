9e3ac1973439f8e274821fb8be007132b4fc5e52
Megan John <meganjohn.791@gmail.com>
2020-02-16 17:31:48 +0000

    First iteration of solution

--- gigasecond/gigasecond.js
+++ gigasecond/gigasecond.js
@@ -3,6 +3,8 @@
 // convenience to get you started writing code faster.
 //
 
-export const gigasecond = () => {
-  throw new Error("Remove this statement and implement this function");
+export const gigasecond = (time) => {
+  var gsAway = new Date();
+  gsAway.setTime(time.getTime() + 1000000000000);
+  return gsAway;
 };
