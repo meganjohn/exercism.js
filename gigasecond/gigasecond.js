2ce96c8968a6eb20f4d787f2a448089127087057
Megan John <meganjohn.791@gmail.com>
2020-02-23 13:41:29 +0000

    New solutions

--- gigasecond/gigasecond.js
+++ gigasecond/gigasecond.js
@@ -2,9 +2,6 @@
 // This is only a SKELETON file for the 'Gigasecond' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
+const GS_in_ms = 10 ** 12;
 
-export const gigasecond = (time) => {
-  var gsAway = new Date();
-  gsAway.setTime(time.getTime() + 1000000000000);
-  return gsAway;
-};
+export const gigasecond = time => new Date(time.getTime() + GS_in_ms);
