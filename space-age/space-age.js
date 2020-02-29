dd867cfc04bee6f240fe24c72a0610f0ea3a4743
Megan John <meganjohn.791@gmail.com>
2020-02-29 10:37:13 +0000

    First iteration of solution

--- space-age/space-age.js
+++ space-age/space-age.js
@@ -2,7 +2,17 @@
 // This is only a SKELETON file for the 'Space Age' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
+const secondsInEarthYear = 31557600;
 
-export const age = () => {
-  throw new Error("Remove this statement and implement this function");
+const secondsInAYear = {
+  "earth": secondsInEarthYear,
+  "mercury": secondsInEarthYear * 0.2408467,
+  "venus": secondsInEarthYear * 0.61519726,
+  "mars": secondsInEarthYear * 1.8808158,
+  "jupiter": secondsInEarthYear * 11.862615,
+  "saturn": secondsInEarthYear * 29.447498,
+  "uranus": secondsInEarthYear * 84.016846,
+  "neptune": secondsInEarthYear * 164.79132
 };
+
+export const age = (planet, seconds) => (seconds/secondsInAYear[planet]).toFixed(2) * 1;
