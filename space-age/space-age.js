9eb7941f9fd210398e84fab078aff0dd85a89590
Megan John <meganjohn.791@gmail.com>
2020-02-29 11:37:11 +0000

    Second iteration of solution

--- space-age/space-age.js
+++ space-age/space-age.js
@@ -4,15 +4,17 @@
 //
 const secondsInEarthYear = 31557600;
 
-const secondsInAYear = {
-  "earth": secondsInEarthYear,
-  "mercury": secondsInEarthYear * 0.2408467,
-  "venus": secondsInEarthYear * 0.61519726,
-  "mars": secondsInEarthYear * 1.8808158,
-  "jupiter": secondsInEarthYear * 11.862615,
-  "saturn": secondsInEarthYear * 29.447498,
-  "uranus": secondsInEarthYear * 84.016846,
-  "neptune": secondsInEarthYear * 164.79132
+const fractionOfEarthYear = {
+  "earth": 1,
+  "mercury": 0.2408467,
+  "venus": 0.61519726,
+  "mars": 1.8808158,
+  "jupiter": 11.862615,
+  "saturn": 29.447498,
+  "uranus": 84.016846,
+  "neptune": 164.79132
 };
 
-export const age = (planet, seconds) => (seconds/secondsInAYear[planet]).toFixed(2) * 1;
+export const age = (planet, seconds) => 
+  (seconds / ( secondsInEarthYear * fractionOfEarthYear[planet] ) )
+  .toFixed(2) * 1;
