c028a5b0ae5bbecc26ce06b962c6f99be420a332
Megan John <meganjohn.791@gmail.com>
2020-05-17 12:09:26 +0100

    Solution passing all tests

--- leap/leap.js
+++ leap/leap.js
@@ -5,10 +5,11 @@
 
 export const isLeap = (year) => {
   let leapYear = false;
-  if (year % 4 === 0
-      && year % 100 !== 0
-      && year % 400 !== 0) {
-        leapYear = true;
-      }
+if ( year % 400 === 0){
+  leapYear = true;
+} else if (year % 4 === 0
+    && year % 100 !== 0) {
+      leapYear = true;
+  }
   return leapYear;
 };
