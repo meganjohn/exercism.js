bca11874aa696ac9ced6f2e5b3e36e952512d346
Megan John <meganjohn.791@gmail.com>
2020-07-10 13:34:07 +0100

    Refactoring to loop through letterSet rather than input string

--- pangram/pangram.js
+++ pangram/pangram.js
@@ -1,10 +1,5 @@
-//
-// This is only a SKELETON file for the 'Pangram' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-// const letterSet = "abcdefghijklmnopqrstuvwxyz".split('');
-const letterSet = "abcdefghijklmnopqrstuvwxyz";
+const letterSet = [..."abcdefghijklmnopqrstuvwxyz"];
+// Alternative: const letterSet = "abcdefghijklmnopqrstuvwxyz".split('');
 
 
 export const isPangram = (inputString) => {
@@ -12,13 +7,12 @@ export const isPangram = (inputString) => {
   let characters = inputString.toLowerCase().split('');
   let letters = [];
 
-  for (let i = 0; i < characters.length; i++) {
-    const character = characters[i];
+  for (let i = 0; i < letterSet.length; i++) {
+    const letter = letterSet[i];
     //Checks if valid lower case character and not already in letters.
-    if ([...letterSet].indexOf(character) >= 0
-        && letters.indexOf(character) === -1
+    if (characters.indexOf(letter) >= 0
       ) {
-        letters.push(character);   
+        letters.push(letter);   
     }
   }
 
