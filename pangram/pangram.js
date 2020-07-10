9861724a60bd08bd3df8ca414e5a559d740f6c88
Megan John <meganjohn.791@gmail.com>
2020-07-10 13:59:43 +0100

    Using every instead of a for loop

--- pangram/pangram.js
+++ pangram/pangram.js
@@ -1,24 +1,7 @@
 const letterSet = [..."abcdefghijklmnopqrstuvwxyz"];
 // Alternative: const letterSet = "abcdefghijklmnopqrstuvwxyz".split('');
 
-
 export const isPangram = (inputString) => {
-  let pangram = false;
-  let characters = inputString.toLowerCase().split('');
-  let letters = [];
-
-  for (let i = 0; i < letterSet.length; i++) {
-    const letter = letterSet[i];
-    //Checks if valid lower case character and not already in letters.
-    if (characters.indexOf(letter) >= 0
-      ) {
-        letters.push(letter);   
-    }
-  }
-
-  if (letters.length === 26) {
-    pangram = true;
-  }
-  
-  return pangram;
+  const characters = inputString.toLowerCase().split('');
+  return letterSet.every(currentLetter => (characters.indexOf(currentLetter) !== -1));
 };
